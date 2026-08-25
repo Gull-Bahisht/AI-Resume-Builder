295be0b3c29437f8e91b1aafe1d29837cba1e924
Gull Bahisht <242188@students.au.edu.pk>
2026-08-26 01:06:32 +0500

    Prepare backend for Vercel

--- server/server.js
+++ server/server.js
@@ -9,31 +9,19 @@ import aiRouter from "./routes/aiRoutes.js";
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-console.log("Mongo URI exists:", !!process.env.MONGODB_URI);
-
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
 
-console.log("Starting server...");
-
-//database connection
-await connectDB()
-
-console.log("Database connection finished");
-
-app.use(express.json())
-
-app.use(cors())
-
-app.get('/',(req,res)=>res.send("Server is live!"))
-
-app.use('/api/users',userRouter)
-
-app.use('/api/resumes',resumeRouter)
-
-app.use('/api/ai',aiRouter)
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Server is live!");
 });
+
+app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
+app.use("/api/ai", aiRouter);
+
+connectDB();
+
+export default app;
