acaf09543d545e95ae10c84bed8ab85e60bfd52a
Gull Bahisht <242188@students.au.edu.pk>
2026-08-26 01:46:33 +0500

    Fix MongoDB connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,42 +1,17 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     mongoose.connection.on("connected", () => {
-//       console.log("Database connected successfully");
-//     });
-
-//     let mongodbURI = process.env.MONGODB_URI;
-
-//     const projectName = "resume-builder";
-
-//     if (!mongodbURI) {
-//       throw new Error("MONGODB_URI env variable not set");
-//     }
-
-//     if (mongodbURI.endsWith("/")) {
-//       mongodbURI = mongodbURI.slice(0, -1);
-//     }
-
-//     await mongoose.connect(`${mongodbURI}/resume-builder`);
-//   } catch (error) {
-//     console.error("Error connecting to mongodb:", error);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose from "mongoose";
-
 
 const connectDB = async () => {
   try {
     const mongodbURI = process.env.MONGODB_URI;
 
-    console.log("URI loaded:", !!mongodbURI);
-    console.log("URI starts with:", mongodbURI?.substring(0, 20));
+    console.log("MongoDB URI loaded:", !!mongodbURI);
+    console.log("MongoDB URI starts with:", mongodbURI?.substring(0, 20));
 
-    await mongoose.connect(`${mongodbURI}/resume-builder`, {
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI environment variable is not set");
+    }
+
+    await mongoose.connect(mongodbURI, {
       serverSelectionTimeoutMS: 10000,
     });
 
